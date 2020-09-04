8f2e73cf8d8fc6e1b6a2fdd2e79bfbb22e44881e
John Orozco <john.o111@yahoo.com>
2020-09-04 17:26:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+def line(katz_deli)
+
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    totalpersons = katz_deli.count
+    i = 0
+    statement = "The line is currently:"
+
+    loop do
+
+      if i < totalpersons
+        person = katz_deli[i]
+        i = i + 1
+        currentperson = "#{i}. #{person}"
+        statement = statement + " " + currentperson
+      else
+        break
+      end
+
+    end
+
+    puts statement
+
+  end
+
+end
+
+def take_a_number(katz_deli, customer)
+  katz_deli.append(customer)
+  i = 0
+  totalpersons = katz_deli.count
+  loop do
+    if i < totalpersons
+      i = i + 1
+      if katz_deli[i] == customer
+        puts "Welcome, #{customer}. You are number #{i} in line."
+      end
+    else
+      break
+    end
+  end
+end
